be1e71b89fb6e86871c668342865807548bd2ae2
amalkrishna <amal.krishna@freecharge.com>
2017-03-26 18:30:02 +0530

    slider img

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -8,7 +8,7 @@ function Slider(props) {
       <Col sm={9}>
         <Carousel id="myCarousel" >
           <Carousel.Item data-slide-number="1">
-            <img width={900} height={480} alt="900x500" src="/images/slider/slider1.png"/>
+            <img width={900} height={480} alt="900x500" src={process.env.PUBLIC_URL + '/images/slider/slider1.png'}/>
             <Carousel.Caption>
               <h1>Replay Jeans Hyperflex</h1>
               <p>Skinny Comfort Ultra Stretch Light Wash</p>
@@ -19,7 +19,7 @@ function Slider(props) {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item data-slide-number="2">
-            <img width={900} height={480} alt="900x500" src="/images/slider/slider1.png"/>
+            <img width={900} height={480} alt="900x500" src={process.env.PUBLIC_URL + '/images/slider/slider1.png'}/>
             <Carousel.Caption>
               <h1>Replay Jeans Hyperflex</h1>
               <p>Skinny Comfort Ultra Stretch Light Wash</p>
@@ -30,7 +30,7 @@ function Slider(props) {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item data-slide-number="3">
-            <img width={900} height={480} alt="900x500" src="/images/slider/slider1.png"/>
+            <img width={900} height={480} alt="900x500" src={process.env.PUBLIC_URL + '/images/slider/slider1.png'}/>
             <Carousel.Caption>
               <h1>Replay Jeans Hyperflex</h1>
               <p>Skinny Comfort Ultra Stretch Light Wash</p>
@@ -41,7 +41,7 @@ function Slider(props) {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item data-slide-number="4">
-            <img width={900} height={480} alt="900x500" src="/images/slider/slider1.png"/>
+            <img width={900} height={480} alt="900x500" src={process.env.PUBLIC_URL + '/images/slider/slider1.png'}/>
             <Carousel.Caption>
               <h1>Replay Jeans Hyperflex</h1>
               <p>Skinny Comfort Ultra Stretch Light Wash</p>
@@ -56,19 +56,19 @@ function Slider(props) {
       <Col sm={3}>
         <ul className="hide-bullets">
             <li className="row" data-target="#myCarousel1" data-slide-to="1">
-                <a className="thumnail over" id="carousel-selector-0"><img src="/images/slider/sl1.png" alt=""/></a>
+                <a className="thumnail over" id="carousel-selector-0"><img src={process.env.PUBLIC_URL + '/images/slider/sl1.png'} alt=""/></a>
             </li>
 
             <li className="row" data-target="#myCarousel1" data-slide-to="2">
-                <a className="thumnail over" id="carousel-selector-1"><img src="/images/slider/sl2.png" alt=""/></a>
+                <a className="thumnail over" id="carousel-selector-1"><img src={process.env.PUBLIC_URL + '/images/slider/sl2.png'} alt=""/></a>
             </li>
 
             <li className="row" data-target="#myCarousel1" data-slide-to="3">
-                <a className="thumnail over" id="carousel-selector-2"><img src="/images/slider/sl3.png" alt=""/></a>
+                <a className="thumnail over" id="carousel-selector-2"><img src={process.env.PUBLIC_URL + '/images/slider/sl3.png'} alt=""/></a>
             </li>
 
             <li className="row" data-target="#myCarousel1" data-slide-to="4">
-                <a className="thumnail over" id="carousel-selector-3"><img src="/images/slider/sl4.png" alt=""/></a>
+                <a className="thumnail over" id="carousel-selector-3"><img src={process.env.PUBLIC_URL + '/images/slider/sl4.png'} alt=""/></a>
             </li>
 
         </ul>
